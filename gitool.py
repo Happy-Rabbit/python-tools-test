c7e0a0aae57bef5ec4eeb905ebe7d5df0ae47645
Happy-Rabbit <zyx.1@outlook.com>
2019-05-06 11:55:43 +0800

    Fixed some small problems

---
+++ gitool.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+from time import strftime, localtime
+from os import system
+from sys import argv
+
+def log():
+    nowTime = strftime("%Y-%m-%d-%H-%M-%S", localtime())
+    fileName = 'log-'+str(nowTime)+'.txt'
+    cmd = 'git log --decorate --oneline --all --graph >> '+fileName
+
+    if system(cmd) == o:
+        print("Finished!")
+    else:
+        print("Error! Unknown Error!")
+
+    exit(0)
+
+def upload(choice=0, lists=[]):
+    if choice == 0:
+        commitNote = str(input("Enter what note you want to announce :\t"))
+    else:
+        commitNote = ' '.join(lists)
+    system('git add *')
+    system(r'git commit -m "%s"' %commitNote )
+    system('git push -u origin master')
+
+if __name__ == '__main__':
+    lists = argv
+    if lists[1] == 'upload':
+        del lists[0]
+        del lists[0]
+        upload(1, lists)
+    elif lists[1] == 'log':
+        log()
+    else:
+        print("Unknown Command!")
+    exit(0)
+
