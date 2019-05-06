d1bb71d0ec27055f010d2880762246c44dbebff4
Happy Rabbit <happy.rabbit.yy@outlook.com>
2019-05-06 17:52:45 +0800

    Added an ability that can compare with the log files

--- gitool.py
+++ gitool.py
@@ -8,31 +8,39 @@ def log():
     nowTime = strftime("%Y-%m-%d-%H-%M-%S", localtime())
     fileName = 'log-'+str(nowTime)+'.txt'
     cmd = 'git log --decorate --oneline --all --graph >> '+fileName
-    fileList = listdir()
-    logList = [i for i in fileList if 'log' in i]
-    sorted(logList)
     if system(cmd) == 0:
         print("Finished!")
     else:
         print("Error! Unknown Error!")
-
+    fileList = listdir()
+    logList = [i for i in fileList if 'log' in i]
+    logList.sort(reverse=True)
+    if len(logList) < 2:
+        pass
+    else:
+        newFile = open(logList[0], 'r+')
+        oldFile = open(logList[1], 'r+')
+        if newFile.read() == oldFile.read():
+            system('rm -rf '+logList[0])
+        else:
+            pass
     exit(0)
 
-def upload(choice=0, lists=[]):
+def upload(choice=0, branchName='master', lists=[]):
     if choice == 0:
         commitNote = str(input("Enter what note you want to announce :\t"))
     else:
         commitNote = ' '.join(lists)
     system('git add *')
     system(r'git commit -m "%s"' %commitNote )
-    system('git push -u origin master')
+    system('git push -u origin '+branchName)
 
 if __name__ == '__main__':
     lists = argv
     if lists[1] == 'upload':
         del lists[0]
         del lists[0]
-        upload(1, lists)
+        upload(1, lists.pop(0), lists)
     elif lists[1] == 'log':
         log()
     else:
